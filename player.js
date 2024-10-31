7ca6182fa9b223b2ed3e3df4a433aa6b9e00a6f7
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-10-31 11:17:58 -0500

    Update player.js

--- player.js
+++ player.js
@@ -3,13 +3,18 @@ function Player(x,y,w,h,img,col){
   this.y = y
   this.w = w
   this.h = h
-  this.img = loadImage(img)
+  //this.img = loadImage(img)
   this.col = col
   
   this.display = function(){
-    pop()
+   
     fill(this.col)
-    rect(this.x, this.y, this.w, this.h )
-    push()
+    rect(this.x, this.y, this.w, this.h)
+    
   }//end display
+  this.update = function(){
+    if(keyIsDown(37)){
+      p.x -= 5;
+    }
+  }
 }//end player
