8eafface527fa9e02b58c1f62b40f22a859d4d0a
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-11-04 10:50:54 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
-function room1(){
-  //Draw the walls for room number1
-  w.push(new Wall(150,150,50,10,"blue"))
-  //for each wall i want to draw
-  //copy and paste line 3, and change the values
-  
+let p 
+//we are making an array that will hold the walls
+let w = []
+
+function setup() {
+  createCanvas(400, 400);
+  p = new Player(200,200,10,10,"","red")
+  room1();
+}
+
+function draw() {
+  background(220);
+  p.display();
+  p.update();
+  for(i = 0;i<w.length;i++){
+    w[i].display();
+  }
 }
