4d7b74983df74598579f5ea24775e11e2c2ece65
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-11-04 11:01:55 -0600

    Update rooms.js

--- rooms.js
+++ rooms.js
@@ -1,6 +1,6 @@
 function room1(){
   //Draw the walls for room number1
-  walls.push(new Wall(150,150,50,10,"blue"))
+  wa.push(new Wall(150,150,50,10,"blue"))
   //for each wall i want to draw
   //copy and paste line 3, and change the values
   
