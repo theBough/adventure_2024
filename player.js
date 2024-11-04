b2a8f73e712667f934dc8c0c94771ec6b757e46a
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-11-04 10:17:19 -0600

    Update player.js

--- player.js
+++ player.js
@@ -5,16 +5,24 @@ function Player(x,y,w,h,img,col){
   this.h = h
   //this.img = loadImage(img)
   this.col = col
+  this.speed = 5
   
   this.display = function(){
-   
     fill(this.col)
     rect(this.x, this.y, this.w, this.h)
-    
   }//end display
   this.update = function(){
     if(keyIsDown(37)){
-      p.x -= 5;
+      p.x -= this.speed;
+    }
+     if(keyIsDown(39)){
+      p.x += this.speed;
+    }
+     if(keyIsDown(38)){
+      p.y -= this.speed;
+    }
+    if(keyIsDown(40)){
+      p.y += this.speed;
     }
   }
 }//end player
