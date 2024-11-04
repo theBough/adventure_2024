3ee760a6dcdf8d01cc4341fa5aeb3efb2cf43bd1
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-11-04 10:38:32 -0600

    Create rooms.js

---
+++ rooms.js
@@ -0,0 +1,7 @@
+function room1(){
+  //Draw the walls for room number1
+  walls.push(new Wall(150,150,50,10,"blue"))
+  //for each wall i want to draw
+  //copy and paste line 3, and change the values
+  
+}
