f774d5bdb056fd58406a675c8f7e8b92195034e7
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-11-04 10:43:49 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,18 @@
 let p 
+//we are making an array that will hold the walls
+let w = []
 
 function setup() {
   createCanvas(400, 400);
-  p = new Player(200,200,50,50,"","red")
+  p = new Player(200,200,10,10,"","red")
+  room1();
 }
 
 function draw() {
   background(220);
   p.display();
-  p.update()
+  p.update();
+  for(i = 0;i<w.length;i++){
+    w[i].display();
+  }
 }
