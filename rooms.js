87ec737bfeba83733e3be02ca0e7cb97462a67b1
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-11-05 10:30:17 -0600

    Update rooms.js

--- rooms.js
+++ rooms.js
@@ -5,3 +5,13 @@ function room1(){
   //copy and paste line 3, and change the values
   
 }
+
+function room2(){
+  //Draw the walls for room number1
+  w.push(new Wall(0,0,20,height-20,"green"))
+  w.push(new Wall(40,40,20,height-20,"green"))
+  w.push(new Wall(40,40,width,20,"green"))
+  //for each wall i want to draw
+  //copy and paste line 3, and change the values
+  
+}
