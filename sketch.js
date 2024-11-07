ada2b042b24c4d33ddc2ada425f02fc3e655ae99
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-11-07 10:35:49 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,9 +10,10 @@ let rooms =[]
 function setup() {
   createCanvas(400, 400);
   p = new Player(200,200,10,10,"","red")
+  // we are starting in room 0, 0 (is the top left room of the grid)
+  //you can change this if you like
   activeRow = 0;
   activeColumn = 0
-  room2();
 }
 
 function draw() {
