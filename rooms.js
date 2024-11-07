643129da5b8089b4c0fb84b5f3f1cd4e4a40b4c0
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-11-07 10:48:31 -0600

    Update rooms.js

--- rooms.js
+++ rooms.js
@@ -1,17 +1,46 @@
-function room1(){
-  //Draw the walls for room number1
-  w.push(new Wall(150,150,50,10,"blue"))
-  //for each wall i want to draw
-  //copy and paste line 3, and change the values
-  
-}
 
+function fillRooms(){
+  rooms = [
+    [room0, room1, room2],
+    [room3, room4, room5],
+    [room6, room7, room8],
+  ]
+}//end Fill Rooms
+function room0(){
+  w=[]
+  w.push(new Wall(150,150,50,10,"yellow"))
+}
+function room1(){
+  w=[]
+  w.push(new Wall(150,150,50,10,"blue"))
+}
 function room2(){
-  //Draw the walls for room number1
+   w=[]
   w.push(new Wall(0,0,20,height-20,"green"))
   w.push(new Wall(40,40,20,height-20,"green"))
-  w.push(new Wall(40,40,width,20,"green"))
-  //for each wall i want to draw
-  //copy and paste line 3, and change the values
-  
+  w.push(new Wall(40,40,width,20,"green"))  
+}
+function room3(){
+  w=[]
+  w.push(new Wall(150,150,50,10,"red"))
+}
+function room4(){
+  w=[]
+  w.push(new Wall(150,150,50,10,"brown"))
+}
+function room5(){
+  w=[]
+  w.push(new Wall(150,150,50,10,"pink"))
+}
+function room6(){
+  w=[]
+  w.push(new Wall(150,150,50,10,"black"))
+}
+function room7(){
+  w=[]
+  w.push(new Wall(150,150,50,10,"violet"))
+}
+function room8(){
+  w=[]
+  w.push(new Wall(150,150,50,10,"magenta"))
 }
