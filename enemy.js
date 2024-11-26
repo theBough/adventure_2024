4c9c03ba0bea51b091c492aac3f3cdf577bc4285
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-11-26 10:53:57 -0600

    Update enemy.js

--- enemy.js
+++ enemy.js
@@ -5,7 +5,7 @@ function Enemy(x, y, w, h, img, col) {
   this.h = h;
   //this.img = loadImage(img)
   this.col = col;
-  this.speed = 3;
+  this.speed = 1;
   this.distance = 100;
   this.traveled = 0;
   this.direction = 1;
@@ -16,13 +16,15 @@ function Enemy(x, y, w, h, img, col) {
   }; //end display
 
   this.update = function () {
-    if(this.traveled < 100){//if it has not traveled max distance
+    console.log(this.traveled);
+    if(this.traveled < this.distance){//if we have not traveled max distance
       this.x += this.direction*this.speed;//move in direction
       this.traveled += this.speed;//accumulate traveled
       } 
-    else if(this.traveled >= 100){ //if we have traveled max distance
-      this.traveled = 0;
-      this.direction = -1;
-    }
-    };
-  }//end enemy
+    else if(this.traveled >= this.distance){//if we have traveled max distance
+      this.traveled = 0;//reset distance traveled
+      this.direction *= -1;//change direction
+    } //if we have traveled max distance
+
+  }
+}//end enemy
