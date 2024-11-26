ce8981e5e9592ccbc699c8bb25206970b7e1ba4a
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-11-26 10:42:57 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,13 +17,13 @@ function Enemy(x, y, w, h, img, col) {
 
   this.update = function () {
     console.log(this.traveled);
-    if(this.traveled < this.distance){//if it has not traveled max distance
+    if(this.traveled < this.distance){//if we have not traveled max distance
       this.x += this.direction*this.speed;//move in direction
       this.traveled += this.speed;//accumulate traveled
       } 
-    else if(this.traveled >= this.distance){
-      this.traveled = 0;
-      this.direction *= -1;
+    else if(this.traveled >= this.distance){//if we have traveled max distance
+      this.traveled = 0;//reset distance traveled
+      this.direction *= -1;//change direction
     } //if we have traveled max distance
 
   }
