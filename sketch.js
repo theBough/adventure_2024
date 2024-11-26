d6b546c87fb332be70cfdba43c57c1b275c9946a
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-11-26 10:41:27 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,41 +1,30 @@
-let p ;
-//we are making an array that will hold the walls
-let w = [];
-//these two variables handle where the player is on the grid
-let activeRow, activeColumn;
-//hold on the rooms
-let rooms =[];
-let k;
-let g
-let e;
+function Enemy(x, y, w, h, img, col) {
+  this.x = x;
+  this.y = y;
+  this.w = w;
+  this.h = h;
+  //this.img = loadImage(img)
+  this.col = col;
+  this.speed = 1;
+  this.distance = 100;
+  this.traveled = 0;
+  this.direction = 1;
 
-function setup() {
-  createCanvas(400, 400);
-  p = new Player(200,200,10,10,"","red")
-  // we are starting in room 0, 0 (is the top left room of the grid)
-  //you can change this if you like
-  activeRow = 0;
-  activeColumn = 0;
-  k = new Key(30,30,10,10,"key/adventure.webp",rooms[0][0],true)
-  g = new Gate(100,100,50,10,"brown",rooms[0][0],true)
-  e = new Enemy(50,50,10,10,"","white")
-}
+  this.display = function () {
+    fill(this.col);
+    rect(this.x, this.y, this.w, this.h);
+  }; //end display
 
-function draw() {
-  background(220);
-  p.display();
-  p.update();
-  for(i = 0;i<w.length;i++){
-    noStroke()
-    w[i].display();
+  this.update = function () {
+    console.log(this.traveled);
+    if(this.traveled < this.distance){//if it has not traveled max distance
+      this.x += this.direction*this.speed;//move in direction
+      this.traveled += this.speed;//accumulate traveled
+      } 
+    else if(this.traveled >= this.distance){
+      this.traveled = 0;
+      this.direction *= -1;
+    } //if we have traveled max distance
+
   }
-  wallCollision()
-  screenChange()
-  rooms[activeRow][activeColumn].call()
-  k.display()
-  g.display();
-  e.display();
-  e.update();
-  keyCollision();
-  gateCollision();
-}
+}//end enemy
