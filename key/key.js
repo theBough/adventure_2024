4d5e7ce629945d2cf6e880e70cbb9d8c1d079e91
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-11-28 11:03:47 -0600

    Update key.js

--- key/key.js
+++ key/key.js
@@ -27,22 +27,22 @@ function keyCollision() {
   
 
     //check if we hit the left of any wall
-    if (p.y <= k.y + k.h && p.y + p.h >= k.y && p.x <= k.x + k.w && p.x >= k.x && k.show) { //if you don't want key to disappear, remove && k.show for ALL if statements
+    if (p.y <= k.y + k.h && p.y + p.h >= k.y && p.x <= k.x + k.w && p.x >= k.x && k.show && k.room == rooms[activeRow][activeColumn]) { //if you don't want key to disappear, remove && k.show for ALL if statements
      k.aquired = true;
     }
 
     //check if we hit the right of any wall
-    if (p.y <= k.y + k.h && p.y + p.h >= k.y && p.x + p.w >= k.x && p.x <= k.x + k.w && k.show) {
+    if (p.y <= k.y + k.h && p.y + p.h >= k.y && p.x + p.w >= k.x && p.x <= k.x + k.w && k.show && k.room == rooms[activeRow][activeColumn]) {
       k.aquired = true;
     }
 
 
-    if (p.x <= k.x + k.w && p.x + p.w >= k.x && p.y <= k.y + k.h && p.y >= k.y && k.show) {
+    if (p.x <= k.x + k.w && p.x + p.w >= k.x && p.y <= k.y + k.h && p.y >= k.y && k.show && k.room == rooms[activeRow][activeColumn]) {
       k.aquired = true;
     }
 
     //check if we hit the top of any wall
-    if (p.x <= k.x + k.w && p.x + p.w >= k.x && p.y + p.h >= k.y && p.y <= k.y + k.h && k.show) {
+    if (p.x <= k.x + k.w && p.x + p.w >= k.x && p.y + p.h >= k.y && p.y <= k.y + k.h && k.show && k.room == rooms[activeRow][activeColumn]) {
       k.aquired = true;
     }
 
