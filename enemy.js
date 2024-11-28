569e4a15f7fa8cb5d844b2f5e31f7abc2989cfdc
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-11-28 10:36:10 -0600

    Update enemy.js

--- enemy.js
+++ enemy.js
@@ -1,19 +1,25 @@
-function Enemy(x, y, w, h, img, col, type) {
+function Enemy(x, y, w, h, img, col, room,show,type) {
   this.x = x;
   this.y = y;
   this.w = w;
   this.h = h;
   //this.img = loadImage(img)
   this.col = col;
+  this.room = room;
   this.type = type;
+  this.show = show;
   this.speed = 1;
   this.distance = 100;
   this.traveled = 0;
   this.direction = 1;
+  this.damage = 1;
 
   this.display = function () {
-    fill(this.col);
-    rect(this.x, this.y, this.w, this.h);
+    if(this.show){
+     fill(this.col);
+     rect(this.x, this.y, this.w, this.h);     
+    }
+
   }; //end display
 
   
@@ -21,18 +27,61 @@ function Enemy(x, y, w, h, img, col, type) {
   //do 'if(type == "name of enemy type")'
   //       write enemy behaviour
   this.update = function () {
-    if(type == "horizontal"){//if enemy type is horizontal movement
-      if(this.traveled < this.distance){//if we have not traveled max distance
-      this.x += this.direction*this.speed;//move in direction
-      this.traveled += this.speed;//accumulate traveled
-      } 
-      else if(this.traveled >= this.distance){//if we have traveled max distance
-        this.traveled = 0;//reset distance traveled
-        this.direction *= -1;//change direction
-      } //if we have traveled max distance
-    } else if(type == "follow"){
-      //covering next class
+    if(this.room == rooms[activeRow][activeColumn] && this.show){//check if the room is correct and enemy is shown
+      if(type == "horizontal"){//if enemy type is horizontal movement
+        if(this.traveled < this.distance){//if we have not traveled max distance
+        this.x += this.direction*this.speed;//move in direction
+        this.traveled += this.speed;//accumulate traveled
+        } 
+        else if(this.traveled >= this.distance){//if we have traveled max distance
+          this.traveled = 0;//reset distance traveled
+          this.direction *= -1;//change direction
+        } //if we have traveled max distance
+      } else if(type == "follow"){ //if follow the player
+        let diffX = p.x - this.x;//x difference between player and enemy
+        let diffY = p.y - this.y;//y difference
+        
+        if(diffX>0){
+          this.x += this.speed;
+          
+        } else if(diffX<0){
+          this.x -= this.speed;
+        }
+        
+        if(diffY>0){
+          this.y += this.speed;
+          
+        } else if(diffY<0){
+          this.y -= this.speed;
+        }
+      }
+      
     }
 
   }
 }//end enemy
+
+//collision with enemy and player
+function enemyCollision() {
+  
+
+    //check if we hit the left of any wall
+    if (p.y <= e.y + e.h && p.y + p.h >= e.y && p.x <= e.x + e.w && p.x >= e.x && e.show) { //if you don't want key to disappear, remove && k.show for ALL if statements
+     k.aquired = true;
+    }
+
+    //check if we hit the right of any wall
+    if (p.y <= e.y + k.h && p.y + p.h >= e.y && p.x + p.w >= e.x && p.x <= e.x + e.w && e.show) {
+    }
+
+
+    if (p.x <= e.x + e.w && p.x + p.w >= e.x && p.y <= e.y + e.h && p.y >= e.y && e.show) {
+
+    }
+
+    //check if we hit the top of any wall
+    if (p.x <= e.x + e.w && p.x + p.w >= e.x && p.y + p.h >= e.y && p.y <= e.y + e.h && e.show) {
+
+    }
+
+}//end colision
